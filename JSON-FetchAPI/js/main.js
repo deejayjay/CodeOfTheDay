b3a3e82f207adc6256c1f259d4f89946851dee69
Deepak Joy Jose <deejayjay@zoho.com>
2021-07-02 13:52:52 -0600

    Adds comments to the code and changes the variable names to more semantice names.

--- JSON-FetchAPI/js/main.js
+++ JSON-FetchAPI/js/main.js
@@ -1,37 +1,44 @@
 window.addEventListener("load", () => {
 	const URL = "/assets/en.json";
 
+	// Fetch JSON data from the file at the given URL
 	fetch(URL)
-		.then(response => response.json())
-		.then(data => DisplayWaitTImes(data))
+		.then(response => response.json()) // returns the JSON body content from the response
+		.then(data => DisplayWaitTimes(data)) // format and display the body content
 		.catch(error => {
 			console.error("Couldn't fetch the data!!!");
 		});
 
-	function DisplayWaitTImes(waitTimesInfo) {
+	// This function accepts the WaitTimes Information in JSON,
+	// then formats it and displays it in the web page
+	function DisplayWaitTimes(waitTimesInfo) {
 		const waitTimes = waitTimesInfo.WaitTimes;
 		let template = "";
 
-		for (const waitTime in waitTimes) {
-			const facilities = waitTimes[`${waitTime}`];
+		// Iterate through each city in the JSON data
+		for (const city in waitTimes) {
+			const facilities = waitTimes[`${city}`];
 
 			template += `
-                        <h2>${capitalizeWord(waitTime)}</h2>
+                        <h2>${capitalizeWord(city)}</h2>
                         <ul>
             `;
 
+			// Display the details of each facility in each city
 			for (const facility in facilities) {
 				const facilityInfo = facilities[`${facility}`];
 				template += `<li>${facilityInfo.name} - ${facilityInfo.note}</li>`;
-				// console.log(facilities[`${facility}`]);
 			}
 			template += `</ul>`;
 		}
+
+		// Insert the parsed JSON data into the .content <div>
 		document
 			.querySelector(".content")
 			.insertAdjacentHTML("beforeend", template);
 	}
 
+	// Capitalize the word passed as argument
 	function capitalizeWord(word) {
 		return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
 	}

2b72b8fccdd6184194f7a5b370bfe655e3f82632
Deepak Joy Jose <deejayjay@zoho.com>
2021-07-09 15:18:43 -0600

    Adds Open Modal and Close Modal functionality

--- Udemy/Modal/starter/script.js
+++ Udemy/Modal/starter/script.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+	btnsOpenModal[i].addEventListener("click", function () {
+		modal.classList.remove("hidden");
+		overlay.classList.remove("hidden");
+	});
+}
+
+btnCloseModal.addEventListener("click", openModal);
+
+document.addEventListener("keydown", closeModal);
+overlay.addEventListener("click", closeModal);
+
+function openModal() {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+}
+
+function closeModal() {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+}

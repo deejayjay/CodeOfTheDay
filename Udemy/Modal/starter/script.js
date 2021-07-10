8e7b50b12d95f89f87ce34781f0195afd32bc9f1
Deepak Joy Jose <deejayjay@zoho.com>
2021-07-10 10:59:13 -0600

    Finishes the modal project.

--- Udemy/Modal/starter/script.js
+++ Udemy/Modal/starter/script.js
@@ -6,23 +6,28 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-	btnsOpenModal[i].addEventListener("click", function () {
-		modal.classList.remove("hidden");
-		overlay.classList.remove("hidden");
-	});
+    btnsOpenModal[i].addEventListener("click", function () {
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    });
 }
 
 btnCloseModal.addEventListener("click", openModal);
-
-document.addEventListener("keydown", closeModal);
 overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        closeModal();
+    }
+});
 
 function openModal() {
-	modal.classList.add("hidden");
-	overlay.classList.add("hidden");
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
 }
 
 function closeModal() {
-	modal.classList.add("hidden");
-	overlay.classList.add("hidden");
+    if (!modal.classList.contains("hidden")) {
+        modal.classList.add("hidden");
+        overlay.classList.add("hidden");
+    }
 }
